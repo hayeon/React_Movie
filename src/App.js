a3a79f0f6aef82f20abb85113babf6080f0a6527
hayeon <hayun4475@gmail.com>
2022-04-14 21:48:14 +0900

    movie api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [loding, setLoding] = useState(true);
+  const [movieList, setMoiveList] = useState([]);
+    useEffect ( () => {
+    fetch ("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")
+    .then( (response) => response.json())
+    .then((json) => setMoiveList(json.data.movies));
+    setLoding(false);
+    
+  }, [] );
+
+  console.log(movieList);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loding ? <h1>loding</h1> : <h1>api~</h1> }
     </div>
   );
 }
